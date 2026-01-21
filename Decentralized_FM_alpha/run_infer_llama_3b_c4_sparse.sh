902451ac6caab0edc1fba1cdc8dad24e138ad912
stark <shrey.0796@gmail.com>
2026-01-21 17:06:58 +0530

    Fix MLP_TOPK: increase from 1000 to 4500 to match training sparsity

--- Decentralized_FM_alpha/run_infer_llama_3b_c4_sparse.sh
+++ Decentralized_FM_alpha/run_infer_llama_3b_c4_sparse.sh
@@ -36,7 +36,7 @@ model_path=./pretrained_models/llama-3.2-3b
 
 # Sparse predictor settings
 export SPARSE_PATH=../checkpoint/llama-3b-sparse-predictor
-export MLP_TOPK=1000           # Number of active neurons (out of 8192)
+export MLP_TOPK=4500           # Number of active neurons (out of 8192) - matches training
 export ATT_TOPK=0.7            # Fraction of active attention heads
 
 # Create directories if needed
